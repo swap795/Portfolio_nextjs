1895175cd6998e76b22162f95643fac0ab1d65ba
swap795 <thapaswapnil08@gmail.com>
2021-10-24 12:44:45 -0600

    make a strings file and change icons

--- pages/about/index.js
+++ pages/about/index.js
@@ -2,12 +2,27 @@ import React from "react";
 
 import AboutMe from "../../app/src/Components/AboutMe";
 
-export default function About() {
+export default function About({ strings }) {
   return (
     <>
-      <AboutMe />
-      <div>Hello</div>
+      <AboutMe strings={strings} />
       {/* TODO: timeline */}
     </>
   );
+}
+
+export async function getStaticProps() {
+  // TODO: once deployed change the URL
+  const aboutMeStrings = await fetch(
+    "http://localhost:3000/api/strings/aboutMeStrings"
+  ).then((res) => res.json());
+  const commonStrings = await fetch(
+    "http://localhost:3000/api/strings/commonStrings"
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      strings: { ...aboutMeStrings, ...commonStrings },
+    },
+  };
 }
