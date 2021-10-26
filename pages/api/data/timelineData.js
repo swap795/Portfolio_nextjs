132503deac6df29943b27f473cae801a917d00ee
swap795 <thapaswapnil08@gmail.com>
2021-10-25 22:34:56 -0600

    refactor timeline

--- pages/api/data/timelineData.js
+++ pages/api/data/timelineData.js
@@ -2,8 +2,9 @@ export default function handler(req, res) {
   res.status(200).json({
     1: {
       id: 1,
-      date: "2016",
+      date: "2015 - 2016",
       title: "Where it all started",
+      heading: "Course",
       description_1: "Senior year of High School",
       description_2:
         "I took an inteoductory class about Computer Engineering. My first taste to the programming world.",
@@ -13,8 +14,9 @@ export default function handler(req, res) {
     },
     2: {
       id: 2,
-      date: "2016",
+      date: "Sep 2016",
       title: "First College Course",
+      heading: "Course",
       description_1: "Decided to major in Computer Engineering.",
       description_2:
         "My coding journey began with Procedural Programming in C++.",
@@ -24,8 +26,9 @@ export default function handler(req, res) {
     },
     3: {
       id: 3,
-      date: "2017",
+      date: "Apr 2017",
       title: "My First Game",
+      heading: "School Project",
       description_1:
         "I took a course on Object Oriented Programming where I built a Moon Lander game. I Utillized object-oriented paradigm and algorithm formulations to implement functionality for the lander.",
       icon: "/assets/cplusplus.png",
@@ -34,19 +37,21 @@ export default function handler(req, res) {
     },
     4: {
       id: 4,
-      date: "2017",
+      date: "Sep 2017",
       title: "My First App",
+      heading: "School Project",
       description_1: "Developed a Board Game Scanner App for Android in Java.",
       description_2:
         "This scanner app could scan the barcode of a given board game and display info such as: Name, Picture, stock availabilty and Price from a database.",
       icon: "/assets/java.png",
       occupation: "education",
-      technologies: ["Java"],
+      technologies: ["Java", "API"],
     },
     5: {
       id: 5,
-      date: "2018",
+      date: "Apr 2018",
       title: "Hardware ...",
+      heading: "School Project",
       description_1: "Part of a team that developed a Home Alarm System",
       description_2:
         "This Home Alarm System is a security feature to put in Homes and also detected motion near the system.",
@@ -55,8 +60,9 @@ export default function handler(req, res) {
     },
     6: {
       id: 6,
-      date: "2019",
+      date: "Jan 2019",
       title: "Discovered a new Passion",
+      heading: "Course",
       description_1: "Took Web Engineering course",
       description_2: "Learned HTML, CSS, vanilla Javascript.",
       icon: "/assets/js.png",
@@ -65,8 +71,9 @@ export default function handler(req, res) {
     },
     7: {
       id: 7,
-      date: "2020",
+      date: "Jan 2020",
       title: "My Portfolio",
+      heading: "Personal Project",
       description_1:
         "After almost a year, I created my Portfolio from the scratch. This website is built in react.",
       icon: "/assets/reactJS.png",
@@ -75,18 +82,20 @@ export default function handler(req, res) {
     },
     8: {
       id: 8,
-      date: "2020",
+      date: "Apr 2020",
       title: "Covid Tracker",
+      heading: "Personal Project",
       description_1:
         "Developed and deployed a responosive COVID-19 tracking webpage.",
       icon: "/assets/reactJS.png",
       occupation: "personal",
-      technologies: ["ReactJS"],
+      technologies: ["ReactJS", "API endpoints"],
     },
     9: {
       id: 9,
-      date: "2020",
-      title: "more Hardware ...",
+      date: "Apr 2020",
+      title: "More Hardware ...",
+      heading: "School Project",
       description_1: "Glove control car",
       description_2:
         "Built an rc car that can be driven by tilting your hands.",
@@ -96,32 +105,51 @@ export default function handler(req, res) {
         "C",
         "Arduino",
         "Bluetooth Module",
-        "MotionTracking MPU-6050",
+        "MPU-6050",
+        "Accelerometer",
+        "Gyroscope",
       ],
     },
     10: {
       id: 10,
-      date: "2021",
+      date: "Jan 2021 - July 2021 (7 months)",
       title: "Internship!!",
+      heading: "Work",
       description_1:
         "Landed a Web Development internship with The Church of Jesus Christ of Latter-day Saints.",
       icon: "/assets/reactJS.png",
       occupation: "work",
-      technologies: ["ReactJS"],
+      technologies: [
+        "ReactJS",
+        "NextJS",
+        "NodeJS",
+        "Agile Software Development",
+        "JIRA",
+      ],
     },
     11: {
       id: 11,
-      date: "2021",
+      date: "2021 - Present",
       title: "First Job!!!",
+      heading: "Work",
       description_1: "I am currently working as a Software Dev Engineer 1.",
       icon: "/assets/nextjs.png",
       occupation: "work",
-      technologies: ["JS", "ReactJS", "Redux", "NextJS", "Firebase"],
+      technologies: [
+        "JS",
+        "ReactJS",
+        "Redux",
+        "Enzyme",
+        "NextJS",
+        "Firebase",
+        "Agile Software Development",
+      ],
     },
     12: {
       id: 12,
-      date: "2021",
+      date: "Oct 2021",
       title: "Time to update my portfolio",
+      heading: "Personal Project",
       description_1: "I rebuilt my Portfolio using nextjs.",
       icon: "/assets/nextjs.png",
       occupation: "personal",
